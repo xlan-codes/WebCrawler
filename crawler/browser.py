494a4137487f23d12d4df97a49f4374fa54ca9ba
Alexis Evelyn <alexis.a.evelyn@gmail.com>
2021-03-13 23:46:27 -0500

    Added Ability to Block URLs

--- crawler/browser.py
+++ crawler/browser.py
@@ -3,6 +3,7 @@
 # Bot Testing Site
 # https://bot.sannysoft.com/
 from typing import Optional, List
+from urllib.parse import urlparse
 
 from easyprocess import EasyProcessError
 from seleniumwire import webdriver
@@ -31,6 +32,9 @@ class Browser:
         self.width: int = 1920
         self.height: int = 1080
 
+        # Domains To Block
+        self.block_list: List[str] = []
+
     def setup_browser(self):
         # Start Headless Display
         try:
@@ -39,10 +43,26 @@ class Browser:
         except EasyProcessError as e:
             self.headless: bool = True
 
-        # Setup Chrome/Chromium
+        # Setup Chrome/Chromium - https://peter.sh/experiments/chromium-command-line-switches/
         self.options = webdriver.ChromeOptions()
         self.options.add_argument('--disable-blink-features=AutomationControlled')  # Disable WebDriver Detection
         self.options.add_argument(f'--user-agent={self.user_agent}')  # To Identify Bot To Server Owners
+
+        # Disable Code That Might Take CPU Cycles
+        self.options.add_argument('--disable-extensions')  # Chrome Apps/Extensions
+        # self.options.add_argument('--disable-plugins')  # No Longer Exists (Used To Be For Flash/Java/etc...)
+        self.options.add_argument('--disable-notifications')  # Disable Website Notification Capabilities
+        self.options.add_argument('--disable-sync')  # Disable Syncing Chrome Account Data
+
+        # Force Lite Mode Pages
+        # self.options.add_argument('--force-enable-lite-pages')  # Force Lite Mode Pages
+
+        # To Disable Accidental Audio Playing
+        self.options.add_argument('--mute-audio')  # Disables Audio Playing
+
+        # self.options.add_argument('--incognito')  # Incognito Mode
+        # self.options.add_argument('--no-referrers')  # Hide Referral Websites
+        # self.options.add_argument('--no-experiments')  # Disable Chrome Flags
         # self.options.add_argument('--remote-debugging-port=9222')  # For Debugging
 
         if self.headless:
@@ -61,9 +81,16 @@ class Browser:
             driver_path: str = normal_driver_path
 
         self.browser = webdriver.Chrome(executable_path=driver_path, options=self.options)
+
+        # Set Default Page Size
         self.browser.set_window_size(width=self.width, height=self.height)
+
+        # Set Timeout To Prevent Hanging On Page
         self.browser.set_page_load_timeout(time_to_wait=self.page_timeout)
         self.browser.set_script_timeout(time_to_wait=self.script_timeout)
+
+        # Intercept Requests To Decide Blocking
+        self.browser.request_interceptor = self._intercept_requests
 
     def quit(self):
         self.browser.quit()
@@ -105,10 +132,24 @@ class Browser:
             self.browser.set_window_size(original_size['width'], original_size['height'])
 
     def can_crawl_now(self, url: str) -> bool:
-        return self.robots.can_crawl_now(url=url)
+        return self.robots.can_crawl_now(url=url) and not self.should_block_request(url=url)
 
     def get_requests(self) -> List[Request]:
         return self.browser.requests
+
+    def _intercept_requests(self, request: Request):
+        # Intercept Network Requests - E.g. Block Ads/Malware
+        if self.should_block_request(url=request.url):
+            request.abort()
+
+    def should_block_request(self, url: str):
+        # Deny Request General Method
+        # I May Add More Advanced Blocking Patterns Later - Potentially To Match AdblockPlus
+        # For Now, We Just Block At The Domain Level
+        if urlparse(url=url).hostname in self.block_list:
+            return True
+
+        return False
 
     def get(self, url: str) -> bool:
         if self.robots.can_crawl_now(url=url):
